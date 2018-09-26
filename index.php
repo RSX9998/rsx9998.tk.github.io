98dcec0a51916b1ac19e928eadc8721eddbf231c
RSX9998 <nikita998x@gmail.com>
2018-09-26 22:05:22 +0300

    Global Update!!!!
    101010011

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>RSX9998.TK</title>
+	<link href='css/css.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	 <style>
+   .kvadrat {
+    border: 1px solid red;
+    padding: 0 10px;
+   }
+
+    A {
+    text-decoration: none; /* Убирает подчеркивание для ссылок */
+    color: red; /* Ссылка красного цвета */
+   } 
+  </style>
+<br><br><br><br><br>
+<div class="kvadrat"><center><h1><font  size="3" color="red" face="Roboto Condensed"> Moscow Date and time:<?php
+echo date( "d.m.y H:i" );?>
+<p>Your IP: <?php
+echo $_SERVER['REMOTE_ADDR']
+?> </p>
+</font></div></center>
+<div class="kvadrat"><center><h1><font size="20" color="red" face="Roboto Condensed">RSX9998.TK</font></h1></center></div>
+<div class="kvadrat">
+<center><table>
+	<tr>
+<td><center><h1><font size="6" color="red" face="Roboto Condensed"><a href="https://twitter.com/rsx9998">Twitter &nbsp; &nbsp;  </a></font></h1></center></td> 
+<td><center><h1><font size="6" color="red" face="Roboto Condensed"><a href="https://github.com/rsx9998">&nbsp; &nbsp;Github</a></font></h1></center></div></td>
+</tr>
+</table></center></div>
+<br><br><br><br><br><br><br><br><br>
+<center><h2><b><font color="red" face="Roboto Condensed">(C)RSX9998</font></center>
+</body>
+</html>
